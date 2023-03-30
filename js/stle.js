158dfc45e6e1327d6125b2deb486a11105fc5e82
hiteshbuha <129034861+hiteshbuha@users.noreply.github.com>
2023-03-30 11:07:00 +0530

    first web

---
+++ js/stle.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+     $('.owl-carousel').owlCarousel({
+          loop:false,
+          margin:10,
+          // nav:true,
+          dots:true,
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:3
+              },
+              1000:{
+                  items:1
+              }
+          }
+      });
+});
